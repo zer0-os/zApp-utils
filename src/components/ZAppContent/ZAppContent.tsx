2f2514a3767b3ec4f08ab90d1c0ad0eacb2f0663
Brett <12437916+colbr@users.noreply.github.com>
2023-11-08 16:18:51 +1300

    feat: add scrollId prop to scroll container

--- src/components/ZAppContent/ZAppContent.tsx
+++ src/components/ZAppContent/ZAppContent.tsx
@@ -1,18 +1,23 @@
-import React, { HTMLProps } from 'react';
+import { HTMLProps } from 'react';
 
 import classNames from 'classnames';
 import styles from './ZAppContent.module.scss';
 
-export type ZAppContentProps = HTMLProps<HTMLDivElement>;
+export interface ZAppContentProps extends HTMLProps<HTMLDivElement> {
+	scrollId?: string;
+}
 
 export const ZAppContent = ({
 	className,
 	children,
+	scrollId,
 	...rest
 }: ZAppContentProps) => {
 	return (
 		<div {...rest} className={classNames(styles.Container, className)}>
-			<div className={styles.Scroll}>{children}</div>
+			<div id={scrollId} className={styles.Scroll}>
+				{children}
+			</div>
 		</div>
 	);
 };
